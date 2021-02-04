70775572ab7a0a437a911013c079018cb6733dd9
sindre <sindreblakseth@gmail.com>
2021-02-04 19:39:29 +0100

    Uncorrected solutions and reference solutions are created successfully, but the correction source terms do not yield the refernce solution at the subsequent time step.

--- datasets.py
+++ datasets.py
@@ -11,6 +11,7 @@ Creating training, validation and test datasets for ML correction of 1D heat con
 
 import matplotlib.pyplot as plt
 import numpy as np
+import numpy_indexed as npi
 import os
 import pickle
 import torch
@@ -39,14 +40,13 @@ def create_datasets():
     """
     # Load pickled simulation data, or create and pickle new data if none exists already.
     pickle_filepath = os.path.join(datasets_location, data_tag + ".pkl")
-    if os.path.exists(pickle_filepath):
+    if os.path.exists(pickle_filepath) and False:
         simulation_data = pickle.load(open(pickle_filepath, "rb"))
     else:
-        # Perform coarse-scale simulation
-        N_t_coarse = int(1/config.dt_coarse) + 1
-        unc_Ts    = np.zeros((N_t_coarse, config.N_coarse + 2))
+        # Perform coarse-scale simulation.
+        unc_Ts    = np.zeros((config.Nt_coarse, config.N_coarse + 2))
         unc_Ts[0] = config.get_T0(config.nodes_coarse)
-        for i in range(1, N_t_coarse):
+        for i in range(1, config.Nt_coarse):
             unc_Ts[i] = physics.simulate(
                 config.nodes_coarse, config.faces_coarse,
                 unc_Ts[i-1], config.T_a, config.T_b,
@@ -54,11 +54,10 @@ def create_datasets():
                 config.dt_coarse, config.dt_coarse, False
             )
 
-        # Perform fine-scale simulation
-        N_t_fine = int(1 / config.dt_fine) + 1
-        ref_Ts = np.zeros((N_t_fine, config.N_fine + 2))
+        # Perform fine-scale simulation.
+        ref_Ts = np.zeros((config.Nt_fine, config.N_fine + 2))
         ref_Ts[0] = config.get_T0(config.nodes_fine)
-        for i in range(1, N_t_fine):
+        for i in range(1, config.Nt_fine):
             ref_Ts[i] = physics.simulate(
                 config.nodes_fine, config.faces_fine,
                 ref_Ts[i - 1], config.T_a, config.T_b,
@@ -66,36 +65,86 @@ def create_datasets():
                 config.dt_fine, config.dt_fine, False
             )
 
-        # Calculate correction source terms
-        sources = np.zeros((N_t_coarse, config.N_coarse))
-        for i in range(1, N_t_coarse):
+        # Store data.
+        simulation_data = {
+            'unc': [unc_Ts, config.nodes_coarse],
+            'ref': [ref_Ts, config.nodes_fine],
+        }
+
+        # Downsample fine-scale data.
+        ref_Ts_downsampled = np.zeros((config.Nt_coarse, config.N_coarse + 2))
+        counter = 0
+        for time_level in range(0, config.Nt_fine, int(config.dt_coarse / config.dt_fine)):
+            idx = npi.indices(np.around(simulation_data['ref'][1], decimals=5),
+                              np.around(simulation_data['unc'][1], decimals=5))
+            for i in range(config.N_coarse + 2):
+                ref_Ts_downsampled[counter][i] = simulation_data['ref'][0][time_level][idx[i]]
+            counter += 1
+
+        # Calculate correction source terms.
+        sources = np.zeros((config.Nt_coarse, config.N_coarse))
+        for i in range(1, config.Nt_coarse):
             sources[i] = physics.get_corrective_src_term(
-                config.nodes_coarse, config.nodes_fine,
-                ref_Ts[int(i*config.dt_coarse/config.dt_fine)], ref_Ts[int((i-1)*config.dt_coarse/config.dt_fine)],
+                config.nodes_coarse, config.faces_coarse,
+                ref_Ts_downsampled[i], ref_Ts_downsampled[i-1],
                 config.T_a, config.T_b,
                 config.get_k, config.get_cV, config.rho, config.A, config.get_q_hat,
                 config.dt_coarse, False
             )
+            corrected = physics.simulate(config.nodes_coarse, config.faces_coarse,
+                                         ref_Ts_downsampled[i-1], config.T_a, config.T_b,
+                                         config.get_k, config.get_cV, config.rho, config.A,
+                                         lambda x: config.get_q_hat(x) + sources[i],
+                                         config.dt_coarse, config.dt_coarse, False)
+            print("Corrected:", corrected)
+            print("Reference:", ref_Ts_downsampled[i])
 
         # Store data
-        simulation_data = {}
-        simulation_data['ref'] = ref_Ts
-        simulation_data['unc'] = unc_Ts
-        simulation_data['src'] = sources
+        simulation_data['src'] = [sources]
         pickle.dump(simulation_data, open(pickle_filepath, "wb"))
 
+    # Downsample fine-scale data.
+    ref_Ts_downsampled = np.zeros((config.Nt_coarse, config.N_coarse + 2))
+    counter = 0
+    for time_level in range(0, config.Nt_fine, int(config.dt_coarse / config.dt_fine)):
+        idx = npi.indices(np.around(simulation_data['ref'][1], decimals=5),
+                          np.around(simulation_data['unc'][1], decimals=5))
+        for i in range(config.N_coarse + 2):
+            ref_Ts_downsampled[counter][i] = simulation_data['ref'][0][time_level][idx[i]]
+        counter += 1
+    simulation_data['ref'] = [ref_Ts_downsampled, config.nodes_coarse]
+
+    # Remove IC from data.
+    simulation_data['unc'][0] = simulation_data['unc'][0][1:,:]
+    simulation_data['ref'][0] = simulation_data['ref'][0][1:,:]
+    simulation_data['src'][0] = simulation_data['src'][0][1:,:]
+
     # Split data into training, validation and test set.
-    train_unc = simulation_data['unc'][:config.N_train_examples]
-    train_ref = simulation_data['ref'][:config.N_train_examples]
-    train_src = simulation_data['src'][:config.N_train_examples]
+    train_unc = simulation_data['unc'][0][:config.N_train_examples,:]
+    train_ref = simulation_data['ref'][0][:config.N_train_examples,:]
+    train_src = simulation_data['src'][0][:config.N_train_examples,:]
 
-    val_unc   = simulation_data['unc'][config.N_train_examples:config.N_train_examples + config.N_val_examples]
-    val_ref   = simulation_data['ref'][config.N_train_examples:config.N_train_examples + config.N_val_examples]
-    val_src   = simulation_data['src'][config.N_train_examples:config.N_train_examples + config.N_val_examples]
+    val_unc   = simulation_data['unc'][0][config.N_train_examples:config.N_train_examples + config.N_val_examples,:]
+    val_ref   = simulation_data['ref'][0][config.N_train_examples:config.N_train_examples + config.N_val_examples,:]
+    val_src   = simulation_data['src'][0][config.N_train_examples:config.N_train_examples + config.N_val_examples,:]
 
-    test_unc  = simulation_data['unc'][config.N_train_examples + config.N_val_examples:]
-    test_ref  = simulation_data['val'][config.N_train_examples + config.N_val_examples:]
-    test_src  = simulation_data['src'][config.N_train_examples + config.N_val_examples:]
+    test_unc  = simulation_data['unc'][0][config.N_train_examples + config.N_val_examples:,:]
+    test_ref  = simulation_data['ref'][0][config.N_train_examples + config.N_val_examples:,:]
+    test_src  = simulation_data['src'][0][config.N_train_examples + config.N_val_examples:,:]
+
+    print("First correction source term of training set:", train_src[0])
+
+    assert train_unc.shape[0] == config.N_train_examples
+    assert train_ref.shape[0] == config.N_train_examples
+    assert train_src.shape[0] == config.N_train_examples
+
+    assert val_unc.shape[0]   == config.N_val_examples
+    assert val_ref.shape[0]   == config.N_val_examples
+    assert val_src.shape[0]   == config.N_val_examples
+
+    assert test_unc.shape[0]  == config.N_test_examples
+    assert test_ref.shape[0]  == config.N_test_examples
+    assert test_src.shape[0]  == config.N_test_examples
 
     # Calculate statistical properties of training data.
     train_unc_mean = np.mean(train_unc)
@@ -107,7 +156,7 @@ def create_datasets():
     train_src_std  = np.std(train_src)
 
     # Augment training data.
-    if "aug" in data_tag:
+    if config.augment_training_data:
         # Shift augmentation.
 
         train_unc_orig = train_unc
@@ -138,24 +187,6 @@ def create_datasets():
         train_src_mirror = -np.flip(train_src, axis=1)
         train_src = np.concatenate((train_src, train_src_mirror), axis=0)
 
-    # Calculate statistical properties of training data.
-    train_unc_mean = np.mean(train_unc)
-    train_ref_mean = np.mean(train_ref)
-    train_src_mean = np.mean(train_src)
-
-    train_unc_std  = np.std(train_unc)
-    train_ref_std  = np.std(train_ref)
-    train_src_std  = np.std(train_src)
-
-    stats = np.asarray([
-        train_unc_mean,
-        train_ref_mean,
-        train_src_mean,
-        train_unc_std,
-        train_ref_std,
-        train_src_std
-    ])
-
     # z_normalize data.
     train_unc_normalized = util.z_normalize(train_unc, train_unc_mean, train_unc_std)
     val_unc_normalized   = util.z_normalize(val_unc,   train_unc_mean, train_unc_std)
@@ -182,12 +213,34 @@ def create_datasets():
     test_ref_tensor  = torch.from_numpy(test_ref_normalized)
     test_src_tensor  = torch.from_numpy(test_src_normalized)
 
-    stats_tensor     = torch.from_numpy(stats)
+    # Create array to store stats used for normalization.
+    stats = np.asarray([
+        train_unc_mean,
+        train_ref_mean,
+        train_src_mean,
+        train_unc_std,
+        train_ref_std,
+        train_src_std
+    ])
+
+    # Pad with zeros to satisfy requirements of Torch's TensorDataset.
+    # (Assumes that all datasets contain 6 or more data examples.)
+    stats_train = np.zeros(train_unc.shape[0])
+    stats_val   = np.zeros(val_unc.shape[0])
+    stats_test  = np.zeros(test_unc.shape[0])
+    stats_train[:6] = stats
+    stats_val[:6]   = stats
+    stats_test[:6]  = stats
+
+    # Convert stats arrays to tensors
+    stats_train_tensor = torch.from_numpy(stats_train)
+    stats_val_tensor = torch.from_numpy(stats_val)
+    stats_test_tensor = torch.from_numpy(stats_test)
 
     # Create datasets.
-    dataset_train = torch.utils.data.TensorDataset(train_unc_tensor, train_ref_tensor, train_src_tensor, stats_tensor)
-    dataset_val   = torch.utils.data.TensorDataset(val_unc_tensor, val_ref_tensor, val_src_tensor, stats_tensor)
-    dataset_test  = torch.utils.data.TensorDataset(test_unc_tensor, test_ref_tensor, test_src_tensor, stats_tensor)
+    dataset_train = torch.utils.data.TensorDataset(train_unc_tensor, train_ref_tensor, train_src_tensor, stats_train_tensor)
+    dataset_val   = torch.utils.data.TensorDataset(val_unc_tensor,   val_ref_tensor,   val_src_tensor,   stats_val_tensor)
+    dataset_test  = torch.utils.data.TensorDataset(test_unc_tensor,  test_ref_tensor,  test_src_tensor,  stats_test_tensor)
 
     return dataset_train, dataset_val, dataset_test
 
@@ -235,46 +288,8 @@ def load_datasets(load_train, load_val, load_test):
 ########################################################################################################################
 
 def main():
-    """
-    plt.figure()
-    plt.title("Temperature profiles from coarse-scale and fine-scale \n discretizations for " + r"$T_L = 250$ and $T_R = 400$")
-    x = np.linspace(0, 1, endpoint = True)
-    plt.plot(x, util.T_coarse(x, 250, 400), label='coarse')
-    plt.plot(x, util.T_exact(x, 250, 400), label='fine')
-    plt.xlabel('x')
-    plt.ylabel('T')
-    plt.legend()
-    plt.grid()
-    plt.savefig('ML_exp1_profiles.pdf')
-    """
-    #if config.data_tag == 'all_black':
-    #    dataset_train, dataset_val, dataset_test = create_colour_datasets(colour = 'black')
-    #elif config.data_tag == 'all_grey':
-    #    dataset_train, dataset_val, dataset_test = create_colour_datasets(colour = 'grey')
-    #elif config.data_tag == 'gradient':
-    #    dataset_train, dataset_val, dataset_test = create_gradient_dataset()
-    #elif config.data_tag == 'flickr_15k':
-    #    dataset_train, dataset_val, dataset_test = create_flickr_dataset()
-    #else:
     dataset_train, dataset_val, dataset_test = create_datasets()
     save_datasets(dataset_train, dataset_val, dataset_test)
-    """
-    dataset_train, _, dataset_test = load_datasets(True, False, True)
-    dataloader_train = torch.utils.data.DataLoader(
-        dataset=dataset_train,
-        batch_size=8,
-        shuffle=True,
-        num_workers=0,
-        pin_memory=True
-    )
-    for i, data in enumerate(dataloader_train):
-        coarse = data[0]
-        fine = data[1]
-        scaled_coarse = util.unnormalize(coarse.data.numpy(), 250, 400)
-        scaled_fine = util.unnormalize(fine.data.numpy(), 250, 400)
-        #print("Coarse:", scaled_coarse)
-        #print("Fine:", scaled_fine)
-    """
 
 ########################################################################################################################
 
