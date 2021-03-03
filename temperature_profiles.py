3987161d5a360d69f431a64e0c2a1d8e01c4a1de
sindre <sindreblakseth@gmail.com>
2021-03-03 17:56:37 +0100

    Updated manufactured solution.

--- temperature_profiles.py
+++ temperature_profiles.py
@@ -24,44 +24,48 @@ import config
 ########################################################################################################################
 
 def main():
-    xs = np.linspace(0,1,1000, endpoint=True)
-    X = np.linspace(0,1,1000, endpoint=True)
-    t = np.linspace(0,5,5000, endpoint=True)
+    xs = np.linspace(0,1,1001, endpoint=True)
+    X = np.linspace(0,1,1001, endpoint=True)
+    t = np.linspace(0,5,5001, endpoint=True)
     X, t = np.meshgrid(X, t)
 
     T_exacts = [
-        lambda x, t: t + 0.5*x**2,
-        lambda x, t: np.sqrt(t) + 10*x**2*(x-1)*(x+2),
+        lambda x, t: t + 0.5*(x**2),
+        lambda x, t: np.sqrt(t) + 10*(x**2)*(x-1)*(x+2),
         lambda x, t: 2*np.sqrt(t) + 7*(x**2)*(x-1)*(x+2),
         lambda x, t: 2*(x**2) - (t**2)*x*(x-1),
         lambda x, t: np.sin(2*np.pi*x)*np.exp(-t),
-        lambda x, t: -2*(x**3)*(x-1)/(t + 0.5),
-        lambda x, t: -(x**3)*(x-1)/(t + 0.1),
+        lambda x, t: -2*(x**3)*(x-1)/(t+0.5),
+        lambda x, t: -(x**3)*(x-1)/(t+0.1),
         lambda x, t: 2 + (x-1)*np.tanh(x/(t+0.1)),
         lambda x, t: np.sin(2*np.pi*t) + np.sin(2*np.pi*x),
-        lambda x, t: np.sin(2*np.pi*t)*np.cos(2*np.pi*x),
-        lambda x, t: np.sin(3*np.pi*t)*np.cos(4*np.pi*x),
-        lambda x, t: np.sin(2*np.pi*x*(t**2)),
+        lambda x, t: 1 + np.sin(2*np.pi*t) * np.cos(2*np.pi*x),
+        lambda x, t: 1 + np.sin(3*np.pi*t) * np.cos(4*np.pi*x),
+        lambda x, t: 1 + np.sin(2*np.pi*x*(t**2)),
         lambda x, t: 5 + x*(x-1)/(t+0.1) + 0.1*t*np.sin(2*np.pi*x),
-        lambda x, t: np.sin(5*x*t)*np.exp(-0.2*x*t),
-        lambda x, t: 5*t*(x**2)*np.sin(10*np.pi*t) + np.sin(2*np.pi*x)/(t+0.2)
+        lambda x, t: 1 + np.sin(5*x*t)*np.exp(-0.2*x*t),
+        lambda x, t: 5*t*(x**2)*np.sin(10*np.pi*t) + np.sin(2*np.pi*x)/(t + 0.2),
+        lambda x, t: 1 + t/(1+((x-0.5)**2)),
+        lambda x, t: 1 + t*np.exp(-1000*(x-0.5)**2)
     ]
     timess   = [
         [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
         [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
         [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
         [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
+        [0, 0.1, 0.2, 0.3, 0.5, 1.0],
         [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
         [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
         [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
         [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
         [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
         [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
-        [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
-        [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
+        [0, 0.1, 0.2, 0.5, 1.0, 1.5, 2.0],
         [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
         [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
         [0, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 3.0, 3.003, 3.01],
+        [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0],
+        [0, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0]
     ]
 
     for i in range(len(T_exacts)):
