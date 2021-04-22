3b8cfc0f051a1fbb5106060cacef5d94a83815fa
sindre <sindreblakseth@gmail.com>
2021-04-22 23:45:48 +0200

    Configured DDM for solution 2.

--- config.py
+++ config.py
@@ -26,9 +26,9 @@ torch.backends.cudnn.benchmark = False
 
 use_GPU    = True
 group_name = "2021-04-22_2D_experiment"
-run_names  = [["2D_GlobalDense_s5_DDM2"]]
-systems    = ["5"]
-data_tags  = ["2D_s5"]
+run_names  = [["2D_GlobalDense_s2_DDM2"]]
+systems    = ["2"]
+data_tags  = ["2D_s2"]
 model_type = 'data'
 model_keys = [5]
 assert len(systems) == len(data_tags) == len(run_names[0])
