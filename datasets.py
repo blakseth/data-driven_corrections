6450f835b4bf1347ed5bb25ea12858fa3ce5f34f
blakseth <54633958+blakseth@users.noreply.github.com>
2021-11-07 19:54:34 +0100

    More code needed updating

--- datasets.py
+++ datasets.py
@@ -99,10 +99,8 @@ def create_parametrized_datasets(cfg):
                 ax.set_ylabel(r'$y$ (m)')
                 ax.label_outer()
                 ax.set(adjustable='box', aspect='equal')
-            fig.colorbar(surf, ax=axs[0, 0])
-            fig.colorbar(im2,  ax=axs[0, 1])
-            fig.colorbar(im3,  ax=axs[1, 0])
-            fig.colorbar(im4,  ax=axs[1, 1])
+            fig.colorbar(surf, ax=axs[0])
+            fig.colorbar(im2,  ax=axs[1])
 
             plt.tight_layout()
             plt.savefig(os.path.join(cfg.run_dir, "src_profiles_alpha" + str(np.around(alpha, decimals=5)) + "t" + str(np.around(cfg.t_end, decimals=5)) + ".pdf"),
